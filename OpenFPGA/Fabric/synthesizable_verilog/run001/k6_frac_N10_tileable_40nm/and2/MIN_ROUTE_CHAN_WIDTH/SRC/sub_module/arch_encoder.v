8dca55862e924d92b1f01a140665437502c9af5b
baruaeee <s.b_04@yahoo.com>
2025-06-14 16:12:29 +0200

    misc.

--- OpenFPGA/Fabric/synthesizable_verilog/run001/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/arch_encoder.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Decoders for fabric configuration protocol
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Sat Jun 14 00:14:30 2025
-//-------------------------------------------
--- OpenFPGA/Fabric/synthesizable_verilog/run001/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/local_encoder.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Local Decoders for Multiplexers
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Sat Jun 14 00:14:30 2025
-//-------------------------------------------
