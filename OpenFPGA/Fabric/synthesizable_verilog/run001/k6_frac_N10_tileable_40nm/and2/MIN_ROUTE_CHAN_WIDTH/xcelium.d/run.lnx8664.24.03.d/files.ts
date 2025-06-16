d399ea9e277530b6cbcf280ef778a09da02a532f
Sajib Barua <bas33767@ei-vm-018.othr.de>
2025-06-17 01:49:31 +0200

    misc.

---
+++ OpenFPGA/Fabric/synthesizable_verilog/run001/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/xcelium.d/run.lnx8664.24.03.d/files.ts
@@ -0,0 +1,2 @@
+1746965572 /home/bas33767/cds.lib
+1750117157 /home/bas33767/Desktop/DD_Lab_exercise/OpenFPGA/Fabric/synthesizable_verilog/run001/k6_frac_N10_tileable_40nm/and2/MIN_ROUTE_CHAN_WIDTH/SRC/and2_include_netlists.v
