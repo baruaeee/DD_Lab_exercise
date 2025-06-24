7d1ab8f880dbaaf60b8973583bd91ad50b5f15ca
baruaeee <s.b_04@yahoo.com>
2025-06-23 20:21:23 -0400

    full pnr flow done

---
+++ OpenFPGA/Fabric/synthesizable_verilog/PNR/.GenusRestruct_innovus.log/genus.cmd
@@ -0,0 +1,9 @@
+# Cadence Innovus(TM) Synthesis Engine-I, Version 23.14-s090_1, built Feb 27 2025 14:28:47
+
+# Date: Mon Jun 23 14:07:12 2025
+# Host: localhost.localdomain (x86_64 w/Linux 4.18.0-553.56.1.el8_10.x86_64) (2cores*2cpus*1physical cpu*Intel(R) Core(TM) i5-5300U CPU @ 2.30GHz 3072KB)
+# OS:   Red Hat Enterprise Linux 8.10 (Ootpa)
+
+encrypt -tcl coeGenusHelpers.tcl > coeGenusHelpers.enc; tcl_source coeGenusInit.tcl
+
+
